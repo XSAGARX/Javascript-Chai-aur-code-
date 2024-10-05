bbbeb81923f9e8c98f1f9ec5d747d67c7fa25304
SAGAR SINGH <90404828+XSAGARX@users.noreply.github.com>
2024-10-05 14:50:16 +0530

    Add files via upload

---
+++ 03_Datatype_conversion.js
@@ -0,0 +1,34 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33 string can be converted in number 
+
+// "33abc" = this can not be converted in number it will show NaN 
+
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 = true; // 0 = false;
+// "" => false 
+// "sagar" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+

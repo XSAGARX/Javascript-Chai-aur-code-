2256f4c9ecb49df488c94498a8e92c2cca12d76e
SAGAR SINGH <90404828+XSAGARX@users.noreply.github.com>
2024-10-05 14:16:29 +0530

    Add files via upload

---
+++ 02_Datatypes_Ecma.js
@@ -0,0 +1,27 @@
+"use strict"; // it treats all js code as a newer versions. 
+
+// alert (3 + 3) we are using node.js, not browser there is different syntax for alert.
+
+console.log (17/
+
+    21+ 
+
+    12
+    ); // you can write in this way but the code readability should be very high. 
+
+
+    let age = 8;
+    let isLoggedIn = false;
+    let state = null;
+
+    // number => 2 to 53
+    // bigInt 
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => value is not assigned 
+    // symbol => used mostly for uniqueness ex: ul/ux designer 
+
+    console.log(typeof undefined);
+    console.log(typeof null);
+    

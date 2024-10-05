dd24eb1d9caab5ec004c7d518fd1d438a4f5c51d
SAGAR SINGH <90404828+XSAGARX@users.noreply.github.com>
2024-10-05 14:17:18 +0530

    Add files via upload

---
+++ 01_Let,Var and const.js
@@ -0,0 +1,23 @@
+const myemail = "sagarsingh3b@gmail.com";
+let minepassword = "1231415";
+var mypassword = "cheentapakdumdum";
+accountCity = "Patna";
+let mystate;
+
+//so here in place of const you cannot write anything. const stands for constant.
+
+// myemail = "crazykiyare@gmal.com"; not allowed because of constant up there. 
+mypassword = "selfie le re";
+my_city = "kiitians";
+console.log(mypassword);
+
+console.table([my_city, mypassword, minepassword, myemail, mystate ]);
+
+/*
+prefer not to use var because of block scope and fucntional scope 
+
+mystate is still undefined because you have not defined it yet. 
+
+*/
+
+

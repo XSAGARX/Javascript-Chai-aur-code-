ff2292ffbe381d474403e6207f216336dcdb2396
SAGAR SINGH <90404828+XSAGARX@users.noreply.github.com>
2024-10-06 09:41:33 +0530

    Add files via upload

---
+++ 08_ComparisonDatatype.js
@@ -0,0 +1,19 @@
+// comparison and equality check they both have different meaning and work 
+
+
+console.log(2 < 1);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+
+// there is only value of clean code so focus more on clean code not on zig zag 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// strict check === here it check the operator as well as data types
+
+console.log("2" === 2);
+
+

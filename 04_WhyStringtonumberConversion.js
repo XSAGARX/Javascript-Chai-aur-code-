80bf414eb40c0f3849e386e42e10d23e483828e6
SAGAR SINGH <90404828+XSAGARX@users.noreply.github.com>
2024-10-06 07:05:17 +0530

    Add files via upload

---
+++ 04_WhyStringtonumberConversion.js
@@ -0,0 +1,30 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Russian";
+let str2 = " Bandana";
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log ("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);// from here strings acts differently 
+// console.log(1 + 2 + "2"); // write code in readable manner dont make it messy 
+
+let gameCounter  = 100
+++gameCounter; // read about prefix and suffix from js mdn.
+
+console.log(gameCounter);
+
+/*
+If one side is a string, the other operand is also converted to a string and they are concatenated.
+If they are both BigInts, BigInt addition is performed. If one side is a BigInt but the other is not, a TypeError is thrown.
+Otherwise, both sides are converted to numbers, and numeric addition is performed.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Division
+*/
+
+

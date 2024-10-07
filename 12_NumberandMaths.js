2f42eaa7ea1a047161542ec568de67c4c816baaf
SAGAR SINGH <90404828+XSAGARX@users.noreply.github.com>
2024-10-07 06:55:28 +0530

    Add files via upload

---
+++ 12_NumberandMaths.js
@@ -0,0 +1,43 @@
+const score = 400
+ console.log (score);
+
+ const balance = new Number (500);
+ console.log(balance);
+
+  //console.log (balance.toString().length);
+  //console.log (balance.toFixed(1));
+
+  const otherNumber = 123.8966
+  //console.log(otherNumber.toPrecision(3));
+
+
+  const hundreds = 1000000;
+  //console.log(hundreds.toLocaleString('en-IN'));
+
+  const lakhs = 30000000000;
+  //console.log(lakhs.toLocaleString('en-IN'));
+
+  //***************************************Maths***********************************************************/
+
+// console.log (Math.abs(-4)); // it just convert -4 to 4 that's it it doesn't do anyother work 
+
+//console.log (Math.round(8.89));
+//console.log (Math.ceil(5.2)); // it chooses upper value 
+// console.log (Math.floor(5.9)); // it chooses lower value 
+
+console.log (Math.random()); // it will generate random number between 0 and 1
+
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log (Math.floor(Math.random()* (max - min + 1))+min);
+
+// practice it more and more and revise it more and more for bettwe quality.
+
+
+
+
+

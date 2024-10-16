1262b16de5cdc5dd6c6d5401c2680e504860a7fe
SAGAR SINGH <90404828+XSAGARX@users.noreply.github.com>
2024-10-16 13:12:55 +0530

    Add files via upload

---
+++ 21_Global_local_scope.js
@@ -0,0 +1,5 @@
+// {} curly braces is only called scope this curly braces is also available in objects 
+// but that is objects decalartion but when it comes with functions and if else then it is called scope. 
+
+
+// there is completely two different scope the scope you are using in vs code and the scope you are using in browser both are different 
